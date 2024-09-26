6c2d44eac185b2c4cf1c6e9f1f82bf4467803d31
nathernandez1189 <123783069+nathernandez1189@users.noreply.github.com>
2024-09-26 02:18:58 -0500

    Ultimos retoques, agregando botón volver al inicio y parte de margenes del texto

--- app.js
+++ app.js
@@ -4,6 +4,37 @@ window.onload = function(event) {
     var app = new App();
     window.app = app;
 }
+// Mostrar el botón de regreso al inicio cuando el usuario hace scroll
+window.onscroll = function() {
+    const backToTop = document.getElementById('back-to-top');
+    if (window.scrollY > 300) {
+        backToTop.classList.add('show');
+    } else {
+        backToTop.classList.remove('show');
+    }
+};
+
+// Animación suave para el botón de "back to top" al hacer clic
+document.getElementById('back-to-top').addEventListener('click', function(event) {
+    event.preventDefault();  // Prevenir el comportamiento por defecto
+    window.scrollTo({
+        top: 0, 
+        behavior: 'smooth'
+    });
+});
+
+
+
+
+// Cambiar el estado activo en el menú al hacer clic
+const menuLinks = document.querySelectorAll('.navigation a');
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', function() {
+        menuLinks.forEach(item => item.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
 
 App.prototype.processingButton = function(event) {
     const btn = event.currentTarget;
